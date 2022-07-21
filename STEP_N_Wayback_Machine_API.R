a30f04fb53395dc080e76221a4604f96c5ea7752
1heidi <oneheidihere@gmail.com>
2022-07-21 10:06:52 +0100

    adding url check scripts and results from run 1

--- STEP_N_Wayback_Machine_API.R
+++ STEP_N_Wayback_Machine_API.R
@@ -1,7 +1,7 @@
-## Purpose: Find if resource url is archived in Wayback Machine
-## Parts: 1) Find Archived URLs from Wayback Machine 2) submit failed URLs to be archived
-## Package(s): wayback, dplyr
-## Input file(s): ner_predictions_all_reshape_2022-07-01.csv
+## Purpose: Check status of extracted urls and retrieve from Wayback Machine urls
+## Parts: 1) Check status of extracted urls 2) find archived URLs in Wayback Machine
+## Package(s): wayback, dplyr, httr, RCurl
+## Input file(s): ner_predictions_sample_2022-07-01.csv
 ## Output file(s):
 ## Notes: wayback package is only on Github https://github.com/hrbrmstr/wayback
 
@@ -10,21 +10,158 @@ library(dplyr)
 library(httr)
 library(RCurl)
 
-##=========================================================##
-###### PART 1: Find Archived URLs from Wayback Machine ###### 
-##=========================================================##
+##=====================================##
+###### PART 1: Retrieve URL Status ###### 
+##=====================================##
 
-pred <- read.csv("ner_predictions_all_reshape_2022-07-01.csv")
-##remove rows without a url
-pred <- pred[(which(nchar(pred$url_1) > 0)),]
-urls <- pred$url_1
+sample <- read.csv("ner_predictions_sample_2022-07-01.csv")
+sample <- sample[(which(nchar(sample$url_1) > 0)),]
+urls <- sample$url_1
+
+## do in batches, takes several hours
+
+urls1 <- urls[1:100]
+urls2 <- urls[101:200]
+urls3 <- urls[201:300]
+urls4 <- urls[301:400]
+urls5 <- urls[401:467]
+
+test <- NULL;
+
+for (i in urls1) {
+  delayedAssign("do.next", {next})
+  r <- tryCatch(RETRY(
+    "GET", i,
+    times = 5,
+    pause_min = 5,
+    pause_base = 2), error = function(cond) {
+      message(paste("URL issue:", i, sep="\n"))
+      message(cond, sep="\n")
+      return(NA)
+      force(do.next)})
+  input_URL <- i
+  returnedURL <- r[[1]]
+  status_code <- if(is.na(r)) {paste0("fail")} else {r[["status_code"]]}
+  report <- cbind(input_URL,returnedURL, status_code)
+  test <- as.data.frame(rbind(test, report))
+}
+
+for (i in urls2) {
+  delayedAssign("do.next", {next})
+  r <- tryCatch(RETRY(
+    "GET", i,
+    times = 5,
+    pause_min = 5,
+    pause_base = 2), error = function(cond) {
+      message(paste("URL issue:", i, sep="\n"))
+      message(cond, sep="\n")
+      return(NA)
+      force(do.next)})
+  input_URL <- i
+  returnedURL <- r[[1]]
+  status_code <- if(is.na(r)) {paste0("fail")} else {r[["status_code"]]}
+  report <- cbind(input_URL,returnedURL, status_code)
+  test <- as.data.frame(rbind(test, report))
+}
+
+for (i in urls3) {
+  delayedAssign("do.next", {next})
+  r <- tryCatch(RETRY(
+    "GET", i,
+    times = 5,
+    pause_min = 5,
+    pause_base = 2), error = function(cond) {
+      message(paste("URL issue:", i, sep="\n"))
+      message(cond, sep="\n")
+      return(NA)
+      force(do.next)})
+  input_URL <- i
+  returnedURL <- r[[1]]
+  status_code <- if(is.na(r)) {paste0("fail")} else {r[["status_code"]]}
+  report <- cbind(input_URL,returnedURL, status_code)
+  test <- as.data.frame(rbind(test, report))
+}
+
+for (i in urls4) {
+  delayedAssign("do.next", {next})
+  r <- tryCatch(RETRY(
+    "GET", i,
+    times = 5,
+    pause_min = 5,
+    pause_base = 2), error = function(cond) {
+      message(paste("URL issue:", i, sep="\n"))
+      message(cond, sep="\n")
+      return(NA)
+      force(do.next)})
+  input_URL <- i
+  returnedURL <- r[[1]]
+  status_code <- if(is.na(r)) {paste0("fail")} else {r[["status_code"]]}
+  report <- cbind(input_URL,returnedURL, status_code)
+  test <- as.data.frame(rbind(test, report))
+}
+
+for (i in urls5) {
+  delayedAssign("do.next", {next})
+  r <- tryCatch(RETRY(
+    "GET", i,
+    times = 5, # the function has other params to tweak its behavior
+    pause_min = 5,
+    pause_base = 2), error = function(cond) {
+      message(paste("URL issue:", i, sep="\n"))
+      message(cond, sep="\n")
+      return(NA)
+      force(do.next)})
+  input_URL <- i
+  returnedURL <- r[[1]]
+  status_code <- if(is.na(r)) {paste0("fail")} else {r[["status_code"]]}
+  report <- cbind(input_URL,returnedURL, status_code)
+  test <- as.data.frame(rbind(test, report))
+}
+
+write.csv(test,"ner_predictions_sample_URL_check_2022-07-21.csv", row.names = FALSE) 
+
+##=========================================================##
+###### PART 2: Find Archived URLs from Wayback Machine ###### 
+##=========================================================##
 
 wayback<- NULL
-for (url in urls) {
+
+for (url in urls1) {
   response <- archive_available(url)
   wayback <- bind_rows(wayback, response)
 }
 
-## get current http status
+for (url in urls2) {
+  response <- archive_available(url)
+  wayback <- bind_rows(wayback, response)
+}
 
+for (url in urls3) {
+  response <- archive_available(url)
+  wayback <- bind_rows(wayback, response)
+}
+
+for (url in urls4) {
+  response <- archive_available(url)
+  wayback <- bind_rows(wayback, response)
+}
+
+for (url in urls5) {
+  response <- archive_available(url)
+  wayback <- bind_rows(wayback, response)
+}
+ 
+## join and analyze
+
+t <- left_join(test, wayback, by = c("input_URL" = "url"))
+t <- select(t, 1, 3, 5, 6)
+names(t)[names(t)=="input_URL"] <- "extracted_url"
+names(t)[names(t)=="status_code"] <- "extracted_url_status"
+names(t)[names(t)=="closet_url"] <- "wayback_url" ## [sic] closet
+names(t)[names(t)=="timestamp"] <- "wayback_url_timestamp"
+
+na <- sum(is.na(t$wayback_url)) 
+
+sum(t$extracted_url_status == 200)
+sum(t$extracted_url_status != 200 & is.na(t$wayback_url))
 
